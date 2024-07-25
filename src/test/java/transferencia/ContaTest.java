4e2b0c1f1bb419452ea5adaf29ae553d62357e18
rafabanderas <rafabanderas@hotmail.com>
2024-07-25 17:49:02 -0300

    First Commit

---
+++ src/test/java/transferencia/ContaTest.java
@@ -0,0 +1,35 @@
+package transferencia;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContaTest {
+    Cliente xuxa;
+    Cliente silvioSantos;
+    Conta contaXuxa;
+    Conta contaSilvio;
+
+    @BeforeEach
+    void setUp() {
+        xuxa = new Cliente("Xuxa", "12345678900", "9876543100");
+        silvioSantos = new Cliente("Silvio Santos", "00123456789", "9987654321");
+
+        contaXuxa = new Conta("0048", "15151", 2500.00, xuxa);
+        contaSilvio = new Conta("0050", "16161", 3500.00, silvioSantos);
+    }
+
+    @Test
+    public void realizarTransacao(){
+        contaXuxa.realizarTransferencia(1000.00, contaSilvio);
+        assertEquals(1500.00, contaXuxa.getSaldo());
+        assertEquals(4500.00, contaSilvio.getSaldo());
+    }
+
+    @Test
+    public void validarTransferenciaInvalida(){
+        boolean resultado = contaXuxa.realizarTransferencia(3500.00, contaSilvio);
+        assertFalse(resultado);
+    }
+
+}
